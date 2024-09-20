9be1679f7d58be97ec98a0f7590898390a95ed02
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-09-20 17:01:45 +0700

    chore: install `@sanity/astro` module
    TODO: Replace strapi with sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sanity({
+      projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+      dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+      useCdn: true,
+    }),
+  ],
 });
