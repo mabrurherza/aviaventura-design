efe329c21dcd8a93efae7789d60acbd3c1110389
mabrurherza <mabrurherza@gmail.com>
2025-02-14 17:37:18 +0700

    revisi 14 Februari 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import sanity from "@sanity/astro";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aviaventura.com/',
   integrations: [tailwind({
     applyBaseStyles: false,
   }), sanity({
     projectId: "jnbqgyyk",
     dataset: "production",
     useCdn: true,
-  }), react()],
+  }), react(), sitemap({
+    // configuration options
+  })],
 });
